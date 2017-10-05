6bce724c8845c8f8c06483675e39aa31a5ab16d2
Tyler Phillips <tylerphillips1@u.boisestate.edu>
2017-10-04 22:58:57 -0600

    Problem 2 done

--- Computing_Foundations_CS507/homework_01/problem_02/include/complexNum.h
+++ Computing_Foundations_CS507/homework_01/problem_02/include/complexNum.h
@@ -1,18 +1,22 @@
-#ifndef complexNum
-#define complexNum
+#ifndef complex_Num
+#define complex_Num
 
-template<class T>
-class complexNum<T> {
+#include <iostream>
+
+using namespace std;
+
+template <class T>
+class complexNum {
 	private:
 		T realNum;
 		T imagNum;
 
 	public:
 		// Constructors and destructor
-		complexNum();
-		complexNum(T num);
-		complexNum(T num1, T num2);
-		~complexNum();
+		complexNum()				: realNum(0),    imagNum(0)    {};
+		complexNum(T num)          : realNum(num),  imagNum(0)    {};
+		complexNum(T num1, T num2) : realNum(num1), imagNum(num2) {};
+		~complexNum() {};
 		
 		// Opertor overloading
 		complexNum operator + (complexNum complex2) {
@@ -22,9 +26,43 @@ class complexNum<T> {
 			return result;
 		}
 
-		// 
-		T get_real();	
-		T get_imag();
+		complexNum operator - (complexNum complex2) {
+			complexNum result;
+			result.realNum = realNum - complex2.get_real();
+			result.imagNum = imagNum - complex2.get_imag();
+			return result;
+		}
+		
+		complexNum operator * (complexNum complex2) {
+			complexNum result;
+			result.realNum = realNum * complex2.get_real();
+			result.imagNum = imagNum * complex2.get_imag();
+			return result;
+		}
+
+		complexNum operator / (complexNum complex2) {
+			complexNum result;
+			result.realNum = realNum / complex2.get_real();
+			result.imagNum = imagNum / complex2.get_imag();
+			return result;
+		}
+
+		complexNum operator % (complexNum complex2) {
+			complexNum result;
+			result.realNum = realNum % complex2.get_real();
+			result.imagNum = imagNum % complex2.get_imag();
+			return result;
+		}
+
+
+		// Methods 
+		T get_real() {
+			return realNum;
+		}
+		T get_imag() {
+			return imagNum;
+		}
+
 };
 
 #endif
--- Computing_Foundations_CS507/homework_01/problem_02/src/main.cpp
+++ Computing_Foundations_CS507/homework_01/problem_02/src/main.cpp
@@ -1,18 +1,42 @@
 #include <iostream>
+#include <cmath>
 #include "complexNum.h"
 
-using namespace std;
+void printNum(complexNum<double> a) {
+	cout << "(" << a.get_real() << ", " << a.get_imag() << "i)";
+}
+
+double abs(complexNum<double> a) {
+	return sqrt(a.get_real() * a.get_real() + a.get_imag() * a.get_imag());
+}
+
 
 int main(void) {
-	
+
 	complexNum<double> a(2.0, 4.0);
 	complexNum<double> b(5.0);
 
-	complexNum<double> c;
+	complexNum<double> c, d, e, f, g;
 
 	c = a + b;
-	
-	cout << "real: " << c.get_real() << " imaginary: " << c.get_imag() << endl;
+	d = a - b;
+	e = a * b;
+	f = a / b;
+	g = a;
 
+	double val = abs(c); 
+
+	cout << "\n*********************************" << endl;
+	cout << "            Problem 2            " << endl;
+	cout << "*********************************" << endl;
+
+	printNum(a); cout  << " + "; printNum(b); cout << " = "; printNum(c); cout << endl;	
+	printNum(a); cout  << " - "; printNum(b); cout << " = "; printNum(d); cout << endl;	
+	printNum(a); cout  << " * "; printNum(b); cout << " = "; printNum(e); cout << endl;	
+	printNum(a); cout  << " / "; printNum(b); cout << " = "; printNum(f); cout << endl;	
+	printNum(g); cout << endl;	
+	cout << "|"; printNum(c); cout << "| = " << val << "\n\n";  	
 	return 0;
 }	
+
+
